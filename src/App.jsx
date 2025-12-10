47e79818be86e5ea933826ca3ebb5f2e6f1ce80c
Rushikesh <rushikeshshinde6200@gmail.com>
2025-12-10 14:29:27 +0530

    creation

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-red-400 font-bold underline">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
